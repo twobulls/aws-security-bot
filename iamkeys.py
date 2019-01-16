84e918e5a3ed75aaa30aff6b26b7c255d48ca4c6
Will McCutcheon <will@twobulls.com>
2019-01-16 16:04:04 -0500

    Moved Bullkit into a class, thus allowing us to not pass `commandargs` so frequently.

--- iamkeys.py
+++ iamkeys.py
@@ -2,36 +2,35 @@
 # -*- coding: utf-8 -*-
 
 import boto3
-import bullkit
 from datetime import datetime, timedelta
 from pytz import timezone
 import yaml
 
-def iamkeys (commandargs):
+def iamkeys (bullkit):
 	expired_keys = {}
 	keys_to_warn = {}
 
 	# Iterate through each IAM user.
-	bullkit.debug('Getting the list of IAM users...', commandargs)
+	bullkit.debug('Getting the list of IAM users...')
 	iam = boto3.resource('iam')
 	for iam_user in iam.users.all():
 		# Iterate through each of the IAM user's access keys.
 		iam_user_name = iam_user.name
-		bullkit.debug('Checking the access keys of: {}'.format(iam_user_name), commandargs)
+		bullkit.debug('Checking the access keys of: {}'.format(iam_user_name))
 		for access_key in iam.User(iam_user_name).access_keys.all():
 			access_key_id = access_key.access_key_id
-			bullkit.debug('Checking access key: {}'.format(access_key_id), commandargs)
+			bullkit.debug('Checking access key: {}'.format(access_key_id))
 
 			# Calculate the age of the keys (converting to UTC where necessary).
 			utcnow = timezone('UTC').localize(datetime.utcnow())
 			access_key_age = utcnow - access_key.create_date
-			bullkit.debug('Access key\'s age: {}'.format(access_key_age), commandargs)
-			access_key_warn_age = timedelta(days = float(commandargs.parse_args().iam_keys_warn_age))
-			access_key_expire_age = timedelta(days = float(commandargs.parse_args().iam_keys_expire_age))
+			bullkit.debug('Access key\'s age: {}'.format(access_key_age))
+			access_key_warn_age = timedelta(days = float(bullkit.commandargs.parse_args().iam_keys_warn_age))
+			access_key_expire_age = timedelta(days = float(bullkit.commandargs.parse_args().iam_keys_expire_age))
 
 			# If the key is approaching expiration...
 			if access_key_age >= access_key_warn_age and access_key_age < access_key_expire_age and access_key.status == 'Active':
-				bullkit.debug('Key {} is approaching expiration.'.format(access_key_id), commandargs)
+				bullkit.debug('Key {} is approaching expiration.'.format(access_key_id))
 				# Assemble the keys into lists with the user name as their key. Each item in the list is a dict containing the key's ID and time until expiration.
 				if not keys_to_warn.get(iam_user_name):
 					keys_to_warn[iam_user_name] = []
@@ -40,7 +39,7 @@ def iamkeys (commandargs):
 
 			# If the key is expired...
 			if access_key_age >= access_key_expire_age and access_key.status == 'Active':
-				bullkit.debug('Key {} is expired.'.format(access_key_id), commandargs)
+				bullkit.debug('Key {} is expired.'.format(access_key_id))
 				# Assemble the keys into lists with the user name as their key.
 				if not expired_keys.get(iam_user_name):
 					expired_keys[iam_user_name] = []
@@ -48,7 +47,7 @@ def iamkeys (commandargs):
 
 	# If we didn't find any access keys that are approaching expiration or have expired...
 	if not keys_to_warn and not expired_keys:
-		bullkit.debug('No issues found with access keys.', commandargs)
+		bullkit.debug('No issues found with access keys.')
 	
 	# If we found access keys that are approaching expiration or have expired...
 	else:
@@ -57,8 +56,8 @@ def iamkeys (commandargs):
 
 		if keys_to_warn:
 			# Format the list into a string for a Slack message.
-			bullkit.debug('Found these access keys that are approaching expiration: {}'.format(keys_to_warn), commandargs)
-			bullkit.debug('Formatting the list of keys that expire soon...', commandargs)
+			bullkit.debug('Found these access keys that are approaching expiration: {}'.format(keys_to_warn))
+			bullkit.debug('Formatting the list of keys that expire soon...')
 			keys_to_warn_list = []
 			for iam_user_name, access_key in keys_to_warn.items():
 				access_key_id = access_key[0]['id']
@@ -69,8 +68,8 @@ def iamkeys (commandargs):
 
 		if expired_keys:
 			# Format the list into a string for a Slack message.
-			bullkit.debug('Found these expired access keys: {}'.format(expired_keys), commandargs)
-			bullkit.debug('Formatting the list of expired access keys...', commandargs)
+			bullkit.debug('Found these expired access keys: {}'.format(expired_keys))
+			bullkit.debug('Formatting the list of expired access keys...')
 			expired_keys_list = []
 			for iam_user_name, access_key in expired_keys.items():
 				expired_key = '{}: {}'.format(iam_user_name, access_key)
@@ -82,15 +81,15 @@ def iamkeys (commandargs):
 		slackmsg = '\n\n'.join(slackmsg_list)
 
 		# If we've been told to post to Slack...
-		if not commandargs.parse_args().no_slack:
+		if not bullkit.commandargs.parse_args().no_slack:
 			# Post the list to the relevant Slack channel.
-			bullkit.debug('Sending the list to Slack...', commandargs)
-			bullkit.send_slack_message(commandargs.parse_args().iam_keys_channel, 'AWS Security Bot', ':robot_face:', slackmsg, commandargs)
+			bullkit.debug('Sending the list to Slack...')
+			bullkit.send_slack_message(bullkit.commandargs.parse_args().iam_keys_channel, 'AWS Security Bot', ':robot_face:', slackmsg)
 
 			# If there are users who need to deactivate their keys and we've been told to nag them...
-			if commandargs.parse_args().iam_keys_nag_users:
+			if bullkit.commandargs.parse_args().iam_keys_nag_users:
 				# Load the map of AWS users to Slack users.
-				bullkit.debug('Trying to load the map of AWS users to Slack users...', commandargs)
+				bullkit.debug('Trying to load the map of AWS users to Slack users...')
 				try:
 					with open('users.yml', 'r') as stream:
 						try:
@@ -98,8 +97,8 @@ def iamkeys (commandargs):
 						except yaml.YAMLError as exc:
 							print(exc)
 				except IOError:
-					bullkit.debug('users.yml can\'t be read, so we\'ll skip messaging Slack users directly.', commandargs)
-				bullkit.debug('Loaded:\n{}'.format(slack_users), commandargs)
+					bullkit.debug('users.yml can\'t be read, so we\'ll skip messaging Slack users directly.')
+				bullkit.debug('Loaded:\n{}'.format(slack_users))
 
 				# If we were able to assemble a map of AWS users to Slack users...
 				if slack_users:
@@ -107,12 +106,12 @@ def iamkeys (commandargs):
 					bad_iam_users = [*keys_to_warn, *expired_keys]
 
 					# Try to message each user directly via Slack.
-					bullkit.debug('Iterating through bad AWS users to see if we can Slack them directly...', commandargs)
+					bullkit.debug('Iterating through bad AWS users to see if we can Slack them directly...')
 					for bad_iam_user in bad_iam_users:
 						if bad_iam_user in slack_users.keys():
 							bad_slack_user = slack_users[bad_iam_user]
 							if bad_slack_user is not False:
-								bullkit.debug('Trying to message @{}'.format(bad_slack_user), commandargs)
+								bullkit.debug('Trying to message @{}'.format(bad_slack_user))
 
 								# Assemble the Slack message.
 								slackmsg_list = []
@@ -130,17 +129,17 @@ def iamkeys (commandargs):
 									slackmsg_list.append('You have the following IAM access key(s) that are expiring soon. You must deactivate them before their stated expiration date.\n```{}```'.format(keys_to_warn_str))
 								slackmsg_list.append('For instructions on deactivating your access keys and replacing them with new ones, please visit http://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_access-keys.html#Using_RotateAccessKey and perform the steps in the section titled: `To rotate access keys without interrupting your applications (console)`')
 								slackmsg = '\n\n'.join(slackmsg_list)
-								bullkit.debug('Message body: {}'.format(slackmsg), commandargs)
+								bullkit.debug('Message body: {}'.format(slackmsg))
 
 								# Send the Slack message.
 								bad_slack_user = slack_users[bad_iam_user]
-								bullkit.send_slack_message('@{}'.format(bad_slack_user), 'AWS Security Bot', ':robot_face:', slackmsg, commandargs)
+								bullkit.send_slack_message('@{}'.format(bad_slack_user), 'AWS Security Bot', ':robot_face:', slackmsg)
 							else:
-								bullkit.debug('Ignoring {} because it\'s set to False in users.yml.'.format(bad_iam_user), commandargs)
+								bullkit.debug('Ignoring {} because it\'s set to False in users.yml.'.format(bad_iam_user))
 						else:
-							bullkit.debug('Couldn\'t find AWS user {} in the user map.'.format(bad_iam_user), commandargs)
+							bullkit.debug('Couldn\'t find AWS user {} in the user map.'.format(bad_iam_user))
 							slackmsg='I don\'t know the Slack name of the AWS user `{}` so I could not send them a message directly. Please update my `users.yml` file so I can message them in the future.'.format(bad_iam_user)
-							bullkit.send_slack_message(commandargs.parse_args().iam_keys_channel, 'AWS Security Bot', ':robot_face:', slackmsg, commandargs)
+							bullkit.send_slack_message(bullkit.commandargs.parse_args().iam_keys_channel, 'AWS Security Bot', ':robot_face:', slackmsg)
 
 		# If we've been told to *not* post to Slack...
 		else:
--- publics3.py
+++ publics3.py
@@ -2,32 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import boto3
-import bullkit
 
-def publics3 (commandargs):
+def publics3 (bullkit):
 	bad_buckets = {}
-	bullkit.debug('Getting the list of S3 buckets...', commandargs)
+	bullkit.debug('Getting the list of S3 buckets...')
 	s3 = boto3.resource('s3')
 	for bucket in s3.buckets.all():
-		bullkit.debug('Checking the ACL of: {}'.format(bucket.name), commandargs)
+		bullkit.debug('Checking the ACL of: {}'.format(bucket.name))
 		for grant in s3.BucketAcl(bucket.name).grants:
 			if grant['Grantee']['Type'] == 'Group' and 'URI' in grant['Grantee'].keys():
 				if grant['Grantee']['URI'] in ['http://acs.amazonaws.com/groups/global/AllUsers', 'http://acs.amazonaws.com/groups/global/AuthenticatedUsers']:
 					if not bad_buckets.get(bucket.name):
-						bullkit.debug('Oh no, it\'s public!', commandargs)
+						bullkit.debug('Oh no, it\'s public!')
 						bad_buckets[bucket.name] = []
 					bad_buckets[bucket.name].append(grant['Permission'])
 
 	# If we didn't find any public buckets...
 	if not bad_buckets:
-		bullkit.debug('Found no public buckets.', commandargs)
+		bullkit.debug('Found no public buckets.')
 		slackmsg = 'No S3 buckets have public permissions. Yay!'
 	
 	# If we found public buckets...
 	else:
 		# Format the list into a string for a Slack message.
-		bullkit.debug('Found these public buckets: {}'.format(bad_buckets), commandargs)
-		bullkit.debug('Formatting the list of bad buckets...', commandargs)
+		bullkit.debug('Found these public buckets: {}'.format(bad_buckets))
+		bullkit.debug('Formatting the list of bad buckets...')
 		bad_buckets_list = []
 		for bucket, privs in bad_buckets.items():
 			bad_bucket = '{}: {}'.format(bucket, privs)
@@ -36,10 +35,10 @@ def publics3 (commandargs):
 		slackmsg = 'The following S3 buckets are public:\n```{}```\nYou should adjust their permissions immediately.'.format(bad_buckets_str)
 
 	# If we've been told to post to Slack...
-	if not commandargs.parse_args().no_slack:
+	if not bullkit.commandargs.parse_args().no_slack:
 		# Post the list to the relevant Slack channel.
-		bullkit.debug('Posting our findings to Slack...', commandargs)
-		bullkit.send_slack_message(commandargs.parse_args().public_s3_channel, 'AWS Security Bot', ':robot_face:', slackmsg, commandargs)
+		bullkit.debug('Posting our findings to Slack...')
+		bullkit.send_slack_message(bullkit.commandargs.parse_args().public_s3_channel, 'AWS Security Bot', ':robot_face:', slackmsg)
 
 	# If we've been told to *not* post to Slack...
 	else:
